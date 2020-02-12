fc6e62b20efa0f6a5aa285541c921c1ec3290c3e
David Parsons <info@dpdesigns.eu>
2020-02-12 13:00:03 +0100

    adding email fields

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hell0");
+    res.send(`<div>
+            <form>
+                <input placeholder="email" value="email" />
+                <input placeholder="password" value="password" />
+                <input placeholder="password confirmation" value="confirmation" />
+                <button >Submit </button>
+            </form>
+
+        </div>`);
 });
 
 app.listen(3000, () => {
