8ec02352dabc177cfdf2a76abbf24418946a787f
David Parsons <info@dpdesigns.eu>
2020-02-13 12:20:09 +0100

    added hashed password with salting

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const crypto = require("crypto");
+const util = require("util ");
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
     constructor(filename) {
@@ -24,13 +27,23 @@ class UsersRepository {
     }
 
     async create(attrs) {
+        // always has an object of attrs = {email: "", password: ""}
         attrs.id = this.randomId();
+
+        //generate a salt to add to the hashed password
+        const salt = crypto.randomBytes(8).toString("hex");
+        //generate a hashed password
+        const buf = await scrypt(attrs.password, salt, 64);
+
         const records = await this.getAll();
-        records.push(attrs);
+
+        //overwrite password with hashed password + salt - seperated by a period
+        const record = { ...attrs, password: `${buf.toString("hex")}.${salt}` };
+        records.push(record);
 
         await this.writeAll(records);
 
-        return attrs;
+        return record;
     }
 
     async writeAll(records) {
