8ad7b206f42d80c8648e904f88aca3fe171ba7b3
David Parsons <info@dpdesigns.eu>
2020-02-17 16:06:11 +0100

    working on edit and delete product pages, some error handling needed

--- routes/admin/productsMain.js
+++ routes/admin/productsMain.js
@@ -8,16 +8,19 @@ var upload = multer({ storage: multer.memoryStorage() });
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const productsIndexTemplate = require("../../views/admin/products/index");
+const productsEditTemplate = require("../../views/admin/products/edit");
 
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
 
+// PRODUCT PAGE //////////////////////////////////////////////////////
 router.get("/admin/products", requireAuthorisation, async (req, res) => {
     const products = await productsRepo.getAll();
     res.send(productsIndexTemplate({ products }));
 });
 
+// NEW PRODUCT PAGE //////////////////////////////////////////////////////
 router.get("/admin/products/new", requireAuthorisation, (req, res) => {
     res.send(productsNewTemplate({}));
 });
@@ -34,6 +37,54 @@ router.post(
         const { title, price } = req.body;
         await productsRepo.create({ title, price, image });
 
+        res.redirect("/admin/products");
+    }
+);
+
+// EDIT PRODUCT PAGE //////////////////////////////////////////////////////
+router.get(
+    "/admin/products/:id/edit",
+    requireAuthorisation,
+    async (req, res) => {
+        const product = await productsRepo.getOne(req.params.id);
+        if (!product) {
+            return res.send(
+                "Sorry, the product you are looking for no longer exists"
+            );
+        }
+        res.send(productsEditTemplate({ product }));
+    }
+);
+
+router.post(
+    "/admin/products/:id/edit",
+    requireAuthorisation,
+    upload.single("image"),
+    [requireTitle, requirePrice],
+    handleErrors(productsEditTemplate, async req => {
+        const product = await productsRepo.getOne(req.params.id);
+        return { product };
+    }),
+    async (req, res) => {
+        const changes = req.body;
+        if (req.file) {
+            changes.image = req.file.buffer.toString("base64");
+        }
+        try {
+            await productsRepo.update(req.params.id, changes);
+        } catch (err) {
+            return res.send("Item could not be found");
+        }
+        res.redirect("/admin/products");
+    }
+);
+
+// DELETE PRODUCT
+router.post(
+    "/admin/products/:id/delete",
+    requireAuthorisation,
+    async (req, res) => {
+        await productsRepo.delete(req.params.id);
         res.redirect("/admin/products");
     }
 );
--- views/admin/products/index.js
+++ views/admin/products/index.js
@@ -1,18 +1,50 @@
 const layout = require("../layout");
 
 module.exports = ({ products }) => {
-    const productList = products
-        .map((product, id) => {
-            return `<div key=${id}>
-                    <h1>${product.title}</h1>
-                    <h2>${product.price}</h2>
-                </div>`;
+    const renderedProducts = products
+        .map(product => {
+            return `
+      <tr>
+        <td>${product.title}</td>
+        <td>${product.price}</td>
+        <td>
+          <a href="/admin/products/${product.id}/edit">
+            <button class="button is-link">
+              Edit
+            </button>
+          </a>
+        </td>
+        <td>
+            <form method="POST" action="/admin/products/${product.id}/delete">
+                <button class="button is-danger">Delete</button>
+            </form>
+        </td>
+
+      </tr>
+
+    `;
         })
         .join("");
 
     return layout({
         content: `
-        <h1 class = "title">Products</h1>
-        ${productList}`
+      <div class="control">
+        <h1 class="subtitle">Products</h1>
+        <a href="/admin/products/new" class="button is-primary">New Product</a>
+      </div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${renderedProducts}
+        </tbody>
+      </table>
+    `
     });
 };
